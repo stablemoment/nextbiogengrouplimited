dcde7e0d83ae945950d67c66865a413aaa1b0bc1
stablemoment <stablemomentltd@gmail.com>
2024-01-07 22:15:51 +0100

    Hope gtag is successfully added

--- src/constant/googleTag.tsx
+++ src/constant/googleTag.tsx
@@ -1,14 +1,16 @@
 const GoogleTag = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
+  <>
+    {/* Google tag (gtag.js) */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-65MCQR3LBT"></script>
+    <script>
+      {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-65MCQR3LBT');
-      `,
-    }}
-  />
+      `}
+    </script>
+  </>
 );
 
 export default GoogleTag;
