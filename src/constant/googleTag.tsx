ceb743fda22ba4584125f1635f49abcf4fa50962
stablemoment <stablemomentltd@gmail.com>
2024-01-07 21:40:00 +0100

    Perfected googleTag Manager

--- src/constant/googleTag.tsx
+++ src/constant/googleTag.tsx
@@ -1,18 +1,14 @@
 import Script from 'next/script';
 
 const GoogleTag = () => (
-  <>
-    <Script src="https://www.googletagmanager.com/gtag/js?id=G-65MCQR3LBT" defer />
-    <Script id="gtag-inline-script">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-65MCQR3LBT');
-      `}
-    </Script>
-  </>
+  <Script strategy="lazyOnload" id="gtag-inline-script">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-65MCQR3LBT');
+    `}
+  </Script>
 );
 
 export default GoogleTag;
