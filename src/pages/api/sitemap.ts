c82f0c976e033baf1dc7c5e93212be5adc02b251
stablemoment <stablemomentltd@gmail.com>
2024-01-09 05:23:25 +0100

    InshaAllah

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -1,11 +1,46 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import generateSitemap from '../sitemap'; // Import the sitemap function
+// api/sitemap.ts
+import { firestore } from '@/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const generateSitemapXML = async (req: NextApiRequest): Promise<string> => {
   try {
-    const data = await generateSitemap(req, res); // Await the result
-    res.status(200).json(data);
+    // Your sitemap generation logic here...
+    const appCollection = collection(firestore, 'properties');
+    const querySnapshot = await getDocs(appCollection);
+  
+    const documents = querySnapshot.docs.map((doc) => ({
+      propertyId: doc.id,
+    }));
+  
+    // Example: Generate a sitemap XML string
+    const sitemapXML = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${documents.map(doc => `
+            <url>
+                <loc>https://${req.headers.host}/properties/${doc.propertyId}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>daily</changefreq>
+                <priority>0.7</priority>
+            </url>`).join('\n')}
+        <!-- Add more URLs as needed -->
+    </urlset>`;
+
+    return sitemapXML;
+  } catch (error) {
+    console.error('Error generating sitemap:', error);
+    throw error;
+  }
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  try {
+    const sitemapXML = await generateSitemapXML(req);
+
+    // Set the Content-Type header to 'text/xml'
+    res.setHeader('Content-Type', 'text/xml');
+
+    // Send the XML sitemap to the browser
+    res.status(200).send(sitemapXML);
   } catch (error) {
     console.error('Error generating sitemap:', error);
     res.status(500).end(); // Respond with a 500 Internal Server Error
